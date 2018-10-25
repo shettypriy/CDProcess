78df3d209ec62e7b0fbbda8c1664e07d69688e6c
shettypriy <pshetty215@gmail.com>
2018-10-25 15:24:01 +0530

    file abc.txt added

--- test.php
+++ test.php
@@ -1,7 +1,10 @@
 Testing from support
 <<<<<<< HEAD
+<<<<<<< HEAD
 Hello world
 hello git
 
+=======
+>>>>>>> parent of e074943... modified test.php file
 =======
 >>>>>>> parent of e074943... modified test.php file
